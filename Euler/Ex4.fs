4100f82dd0e1b64c094c598b99d1c7309ec86351
antony kakavas <antonykak@hotmail.com>
2023-05-03 21:28:15 +0200

    Added ex4

--- Euler/Ex4.fs
+++ Euler/Ex4.fs
@@ -1,12 +1,19 @@
 namespace Euler
 
-open System.Linq
-
 module Ex4 = 
-    let indexOf  sequence value  = sequence |> Seq.findIndex (fun x -> x=value)
     let isPalindrome (number:int )= 
                         let stringNum = string number  
                         let charArray  = stringNum.ToCharArray()
                         let reverse  = Array.rev charArray
                         charArray |> Seq.forall2  (fun x y -> x=y) reverse
-    
+    let comb lst =
+            let combHelper el lst =
+                lst |> List.map (fun lstEl -> el::[lstEl])
+            let filterOut el lst =
+                lst |> List.filter (fun lstEl -> lstEl <> el)
+            lst |> List.map (fun lstEl -> combHelper lstEl (filterOut lstEl lst)) 
+                |> List.concat 
+                |> List.map (fun x -> x[0]*x[1]) 
+                |> List.where (fun x -> isPalindrome x)
+                |> List.sort
+    List.ofSeq {999..-1..100} |> comb |> List.rev |> List.head |> printfn "%d"
